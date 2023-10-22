b9927afc2618cea2c38f4dd9779f639330dc6eaf
Onur Ulusoy <onurulusoys4@gmail.com>
2023-10-22 21:42:37 +0300

    Create initial map on gazebo

--- routewise_ws/src/rw_simulation/setup.py
+++ routewise_ws/src/rw_simulation/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/gazebo_maps', ['rw_simulation/gazebo_maps/initial_map']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
