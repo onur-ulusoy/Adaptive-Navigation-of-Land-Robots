e800321f6bfe877947f34eef4cf6d8d964c3c926
Onur Ulusoy <onurulusoys4@gmail.com>
2023-10-29 00:40:22 +0300

    Resolve loading issues in Mecanum drive plugin

--- routewise_ws/src/rw_gz_plugins/src/mecanum_drive.cpp
+++ routewise_ws/src/rw_gz_plugins/src/mecanum_drive.cpp
@@ -3,74 +3,96 @@
 #include <gazebo/transport/transport.hh>
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include <gazebo/gazebo.hh>
 
-namespace gazebo {
+#include <iostream>
+using namespace std;
 
-class MecanumDrivePlugin : public ModelPlugin {
+namespace gazebo
+{
+  class MecanumDrive : public ModelPlugin
+  {
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+    {
+      cout << "MecanumDrive plugin is loaded." << endl;
 
-public:
-  void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) override {
-    model = _model;
-    node = transport::NodePtr(new transport::Node());
-    node->Init(model->GetWorld()->Name());
-    
-    // Initialize ROS 2 node
-    rclcpp::init(0, nullptr);
-    ros_node = rclcpp::Node::make_shared("mecanum_drive_plugin");
+      // Store the model pointer
+      this->model = _parent;
+      
+      // Create ROS node
+      this->node = std::make_shared<rclcpp::Node>("mecanum_drive");
+      
+      // Get the wheel joints
+      front_left_wheel = model->GetJoint("front_left_wheel_joint");
+      front_right_wheel = model->GetJoint("front_right_wheel_joint");
+      rear_left_wheel = model->GetJoint("rear_left_wheel_joint");
+      rear_right_wheel = model->GetJoint("rear_right_wheel_joint");
 
-    // Get the wheel joints
-    front_left_wheel = model->GetJoint("front_left_wheel_joint");
-    front_right_wheel = model->GetJoint("front_right_wheel_joint");
-    rear_left_wheel = model->GetJoint("rear_left_wheel_joint");
-    rear_right_wheel = model->GetJoint("rear_right_wheel_joint");
+      // Ensure the joints are available
+      if (!front_left_wheel || !front_right_wheel || !rear_left_wheel || !rear_right_wheel) {
+        cout << "Missing wheel joint(s)" << endl;
+        return;
+      }
 
-    // Ensure the joints are available
-    if (!front_left_wheel || !front_right_wheel || !rear_left_wheel || !rear_right_wheel) {
-      RCLCPP_FATAL(ros_node->get_logger(), "Missing wheel joint(s)");
-      return;
+      // Get robot dimensions from SDF
+      if (_sdf->HasElement("robotWidth")) {
+        robot_width = _sdf->Get<double>("robotWidth");
+      } else {
+          cout << "No robotWidth specified. Using default value of 0.5." << endl;
+          robot_width = 0.5;
+      }
+
+      if (_sdf->HasElement("robotLength")) {
+        robot_length = _sdf->Get<double>("robotLength");
+      } else {
+          cout << "No robotLength specified. Using default value of 0.4." << endl;
+          robot_length = 0.4;
+      }
+
+      // Setup ROS 2 subscription
+      this->cmd_vel_sub = this->node->create_subscription<geometry_msgs::msg::Twist>(
+        "/cmd_vel", 10, std::bind(&MecanumDrive::OnCmdVel, this, std::placeholders::_1)
+      );
+          
+      // Initialize update connection
+      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+          std::bind(&MecanumDrive::OnUpdate, this));
     }
 
-    // Setup ROS 2 subscription
-    cmd_vel_sub = ros_node->create_subscription<geometry_msgs::msg::Twist>(
-      "/cmd_vel", 10, std::bind(&MecanumDrivePlugin::OnCmdVel, this, std::placeholders::_1)
-    );
+    void OnCmdVel(const geometry_msgs::msg::Twist::SharedPtr msg) {
+      vx = msg->linear.x;
+      vy = msg->linear.y;
+      wz = msg->angular.z;
+    }
 
-    // Connect to the world update event
-    update_connection = event::Events::ConnectWorldUpdateBegin(
-      std::bind(&MecanumDrivePlugin::OnUpdate, this));
-  }
+    public: void OnUpdate()
+    {
+      double v_rot = wz * ((robot_width + robot_length) / 2);
 
-  void OnCmdVel(const geometry_msgs::msg::Twist::SharedPtr msg) {
-    vx = msg->linear.x;
-    vy = msg->linear.y;
-    wz = msg->angular.z;
-  }
+      // Implement Mecanum kinematics to compute wheel velocities
+      double front_left_vel = vx - vy - v_rot;
+      double front_right_vel = vx + vy + v_rot;
+      double rear_left_vel = vx + vy - v_rot;
+      double rear_right_vel = vx - vy + v_rot;
 
-  void OnUpdate() {
-    // Implement Mecanum kinematics to compute wheel velocities
-    double front_left_vel = vx - vy - wz;
-    double front_right_vel = vx + vy + wz;
-    double rear_left_vel = vx - vy + wz;
-    double rear_right_vel = vx + vy - wz;
+      // Set wheel velocities
+      front_left_wheel->SetVelocity(0, front_left_vel);
+      front_right_wheel->SetVelocity(0, front_right_vel);
+      rear_left_wheel->SetVelocity(0, rear_left_vel);
+      rear_right_wheel->SetVelocity(0, rear_right_vel);
+    }
 
-    // Set wheel velocities
-    front_left_wheel->SetVelocity(0, front_left_vel);
-    front_right_wheel->SetVelocity(0, front_right_vel);
-    rear_left_wheel->SetVelocity(0, rear_left_vel);
-    rear_right_wheel->SetVelocity(0, rear_right_vel);
-  }
+    private:
 
-private:
-  transport::NodePtr node;
-  physics::ModelPtr model;
-  physics::JointPtr front_left_wheel, front_right_wheel, rear_left_wheel, rear_right_wheel;
-  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub;
-  rclcpp::Node::SharedPtr ros_node;
-  event::ConnectionPtr update_connection;
-  double vx = 0, vy = 0, wz = 0;
+    rclcpp::Node::SharedPtr node;
+    physics::ModelPtr model;
+    physics::JointPtr front_left_wheel, front_right_wheel, rear_left_wheel, rear_right_wheel;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub;
+    event::ConnectionPtr updateConnection;
+    double vx = 0, vy = 0, wz = 0;
+    double robot_width, robot_length;
+  };
 
-};
-
-GZ_REGISTER_MODEL_PLUGIN(MecanumDrivePlugin)
-
-}  // namespace gazebo
+  // Register this plugin with the simulator
+  GZ_REGISTER_MODEL_PLUGIN(MecanumDrive)
+}
